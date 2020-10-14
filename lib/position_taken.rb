4a0dcf6f21f060630057468c51d1c3af61303bd3
Ryna Truong <rynatruong@outlook.com>
2020-10-14 15:58:51 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,8 @@
 # code your #position_taken? method here!
 
 def position_taken?(board, index)
-  if board[index] != " " || "" || nil
-    return false
+  if board[index] == "X" || "O"
+    return true
   else 
     true
   end
